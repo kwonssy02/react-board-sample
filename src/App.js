64bd7d311f31426f1effb2950654ad9177587afa
HYUK CHAN KWON <KHC@HYUKui-MacBook-Pro.local>
2018-08-01 23:27:20 +0900

    app.js 내용 변경

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './assets/css/bootstrap.min.css';
-import './assets/css/bootstrap-theme.min.css';
-import './assets/css/custom.css';
 
 class App extends Component {
   render() {
     return (
       <div className="wrapper">
 
-        {/* Sidebar */}
+        {/* 사이드바 */}
         <nav id="sidebar">
           
           <h3>Bootstrap Sidebar</h3>
@@ -28,35 +25,9 @@ class App extends Component {
           </ul>
         </nav>
 
-        {/* Page Content */}
+        {/* 페이지 내용 */}
         <div id="content">
             <h3>게시물 목록</h3>
-            <table className="table">
-              <thead>
-                <tr>
-                  <td>No</td>
-                  <td>제목</td>
-                  <td>작성자</td>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>안녕하세요</td>
-                  <td>권혁찬</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>반갑습니당</td>
-                  <td>여현승</td>
-                </tr>
-                <tr>
-                  <td>3</td>
-                  <td>와 폭염</td>
-                  <td>전유민</td>
-                </tr>
-              </tbody>
-            </table>
         </div>
 
     </div>  
