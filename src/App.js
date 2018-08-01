2ed45323bb987b5b04a2857ca10c94d5fe9fa132
HYUK CHAN KWON <KHC@HYUKui-MacBook-Pro.local>
2018-08-01 23:40:58 +0900

    NodeList 파일 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 import './assets/css/bootstrap.min.css';
 import './assets/css/bootstrap-theme.min.css';
 import './assets/css/custom.css';
+
+import NoticeList from './views/NoticeList.js';
 
 class App extends Component {
   render() {
@@ -30,33 +31,7 @@ class App extends Component {
 
         {/* Page Content */}
         <div id="content">
-            <h3>게시물 목록</h3>
-            <table className="table">
-              <thead>
-                <tr>
-                  <td>No</td>
-                  <td>제목</td>
-                  <td>작성자</td>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>안녕하세요</td>
-                  <td>권혁찬</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>반갑습니당</td>
-                  <td>여현승</td>
-                </tr>
-                <tr>
-                  <td>3</td>
-                  <td>와 폭염</td>
-                  <td>전유민</td>
-                </tr>
-              </tbody>
-            </table>
+            <NoticeList />
         </div>
 
     </div>  
