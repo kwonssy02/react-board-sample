d645a2c6f9725c8983356a91b12c433e6a553205
HYUK CHAN KWON <KHC@HYUKui-MacBook-Pro.local>
2018-11-04 20:52:41 +0900

    Updates

--- precache-manifest.728563851b343f862784352f716fa655.js
+++ precache-manifest.728563851b343f862784352f716fa655.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/react-board-sample/static/js/runtime~main.b2faf094.js"
   },
   {
-    "revision": "fd89f3d2bfbf0f753824",
-    "url": "/react-board-sample/static/js/main.fd89f3d2.chunk.js"
+    "revision": "8de8445f5b600f0ce76d",
+    "url": "/react-board-sample/static/js/main.8de8445f.chunk.js"
   },
   {
     "revision": "9d78c0d31681806969b7",
     "url": "/react-board-sample/static/js/1.9d78c0d3.chunk.js"
   },
   {
-    "revision": "fd89f3d2bfbf0f753824",
-    "url": "/react-board-sample/static/css/main.30bb522b.chunk.css"
+    "revision": "8de8445f5b600f0ce76d",
+    "url": "/react-board-sample/static/css/main.f9ef86d8.chunk.css"
   },
   {
     "revision": "9d78c0d31681806969b7",
     "url": "/react-board-sample/static/css/1.0a4f3a38.chunk.css"
   },
   {
-    "revision": "f8299e991d527ab922050a9fcfa3c08d",
+    "revision": "278573bae4f17aa3b7d55f5cec9191b4",
     "url": "/react-board-sample/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-board-sample/precache-manifest.43c7313e242c3dc86c74f6b52ff484b4.js"
+  "/react-board-sample/precache-manifest.728563851b343f862784352f716fa655.js"
 );
 
 workbox.clientsClaim();
